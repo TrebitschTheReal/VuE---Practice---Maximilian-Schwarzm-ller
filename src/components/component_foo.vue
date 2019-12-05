f6b9efe101808919128cb6b5ff13e0be073dcf8b
Viktor Dohanyos <dohanyos.viktor@innostart.hu>
2019-12-05 13:18:45 +0100

    hello commit

---
+++ src/components/component_foo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+     <button class="btn btn-primary">Liter</button>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String
+  }
+}
+</script>
