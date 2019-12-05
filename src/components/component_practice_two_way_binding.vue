ce76cc7b7e4206efd0d028477bb8d090cd6ff5aa
Viktor Dohanyos <dohanyos.viktor@innostart.hu>
2019-12-05 15:25:02 +0100

    two way binding

--- src/components/component_practice_two_way_binding.vue
+++ src/components/component_practice_two_way_binding.vue
@@ -3,8 +3,18 @@
       <p>component practice - use two way binding</p>
       <!-- 5) Pre-Populate this input with your name (set the "value" attribute) -->
       <div class="row">
-         <input class="col mx-1" type="text" v-model="text"/>
-         <input disabled class="col mx-1" type="text" :value="text"/>
+         <input class="col m-1" type="text" v-model="text" placeholder="Type here"/>
+         <input disabled class="col m-1" type="text" :value="text"/>
+      </div>
+      <div class="row">
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
+         <textarea disabled class="col m-1" name="" cols="10" rows="5" v-model="text"></textarea>
       </div>
    </div>
 </template>
